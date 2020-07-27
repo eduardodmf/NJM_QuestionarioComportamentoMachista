c4adb90d40300ddbf93bd9c1369498125a95e2f7
Eduardo Deodoro de Moraes Florindo <eduardodmf@outlook.com>
2020-07-27 12:27:32 -0300

    Add project files.

---
+++ Content/js/questionario.js
@@ -0,0 +1,133 @@
+function buscarTextoInfoPergunta(numPergunta) {
+    var textoInformacao = "";
+    switch (numPergunta) {
+        case 1:
+            textoInformacao =
+                "Muitos homens se dizem contra o machismo, e acreditam na igualdade entre homens e mulheres e ficam " +
+                "horrorizados com veem feminicídios no noticiário. Mas a violência contra a mulher começa em pequenas " +
+                "atitudes de desrespeito e desprezo, uma parte da nossa cultura que não vai mudar sozinha se a gente " +
+                "não se manifestar ativamente contra. E isso significa ser aquele cara do contra que puxa a orelha do " +
+                "broder quando ele faz aquela piada machista. Leia mais sobre isso clicando " +
+                "<a href='http://www.compromissoeatitude.org.br/feminicidio-a-ultima-etapa-do-ciclo-da-violencia-contra-a-mulher-por-lourdes-bandeira/' target='_blank' class='linkModalInfoPergunta'> " +
+                    "aqui." +
+                "</a>";
+            break;
+        case 2:
+            textoInformacao =
+                "A responsabilidade pelos cuidados da casa, filhos, etc, com frequência cai nos ombros das mulheres, mesmo " +
+                "quando as mulheres trabalham fora de casa, criando uma carga extra que convencionou-se chamar 'Dupla jornada'. " +
+                "No entanto, essa distribuição desigual não é natural, nem imutável. Basta ver que homens são igualmente capazes " +
+                "de exercer tarefas domésticas, cuidar de doentes, criar filhos e qualquer outra tarefa que se atribui à uma mulher. " +
+                "De fato, antropologos como Margaret Mead (<a href='https://pt.wikipedia.org/wiki/Margaret_Mead' target='_blank' class='linkModalInfoPergunta'>https://pt.wikipedia.org/wiki/Margaret_Mead</a>) identificaram diversas situaçoes " +
+                "em que tarefas associadas com masculino e feminino em uma cultura são atribuídas a ou outro.Essas distribuições são " +
+                "arbitrárias e podem ser revistas, especialmente quando estão causando sofrimento ou injustiças.";
+            break;
+        case 3:
+            textoInformacao =
+                "Muitos homens parecem acreditar que as mulheres não sabem tanto quanto eles sobre qualquer assunto. " +
+                "A época em que as mulheres não podiam estudar já passou faz tempo.Tem mulher em todas as áreas do conhecimento, " +
+                "então antes de esbanjar seus conhecimentos, tenha certeza que a sua explicação é necessária-ou desejada. Ter o seu " +
+                "conhecimento subestimado por homens é tão comum para as mulheres que o fenômeno ganhou um nome: Mansplaining " +
+                "(algo como 'explicação do homem'). Para saber mais sobre o assunto, clique " +
+                "<a href='https://revistamarieclaire.globo.com/Comportamento/noticia/2019/03/mansplaining-ou-nem-sempre-sutil-maneira-de-calar-uma-mulher.html;' target='_blank' class='linkModalInfoPergunta'>aqui</a>.";
+            break;
+        case 4:
+            textoInformacao =
+                "Primeiro, pra uma paquera ser boa, tem que ser boa para os dois lados. Só quem está sendo ofendido sabe dizer se " +
+                "algo é inofensivo ou não.Então se uma mulher reclama de um elogio, é MUITO importante parar e ouvir o incômodo dela. " +
+                "Se ela está incomodada, a boa intenção ('é só um elogio') pode estar encobrindo um assédio - e se eu estou ignorando a " +
+                "vontade dela, tratando o meu desejo como mais importante do que o dela, é exatamente isso que está acontecendo. " +
+                "Tente se colocar no lugar dela. A gente vive em uma sociedade em que as mulheres são frequentemente desrespeitadas e até " +
+                "agredidas na rua e em casa. Vamos fazer nossa parte para diminuir esse mal estar? " +
+                "Para saber mais, clique <a href='https://revistaforum.com.br/blogs/ativismodesofa/eu-tenho-voz-e-digo-cantada-nao-e-elogio/' target='_blank' class='linkModalInfoPergunta'>aqui</a> " +
+                "e/ou <a href='https://www.instagram.com/tv/B1T_aSuAP_7/?igshid=1nzew80hbk044' target='_blank' class='linkModalInfoPergunta'>aqui</a>.";
+            break;
+        case 5:
+            textoInformacao =
+                "Examine a sua irritação nesse tipo de situação. Fomos criados em uma sociedade em que o homem tem a última palavra. " +
+                "Por trás disso, pode estar embutido uma ideia de hierarquia, ou seja, de que as opiniões e vontades dos homens " +
+                "valem mais do que as da mulher. Se você está se sentindo irritado com uma situação na sua relação, procure formas " +
+                "não-violentas de lidar com isso e, se não conseguir, procure ajuda. Para ler mais, clique " +
+                "<a href='https://brasil.elpais.com/brasil/2018/03/07/politica/1520426823_220468.html' target='_blank' class='linkModalInfoPergunta'>aqui</a>.";
+            break;
+        case 6:
+            textoInformacao =
+                "Várias situações de violência doméstica contra a mulher começam com comportamentos controladores, e o próprio " +
+                "comportamento controlador pode ser considerado uma forma de violência psicológica. No final das contas, todo mundo " +
+                "pode escolher suas roupas, suas companhias e os lugares onde quer ir. Para saber mais, clique " +
+                "<a href='https://youtu.be/I-3ocjJTPHg' target='_blank' class='linkModalInfoPergunta'>aqui</a>.";
+            break;
+        case 7:
+            textoInformacao =
+                "Essas frases preocupam porque podem fazer as mulheres duvidarem de si mesmas, causar sofrimento psicológico e " +
+                "várias consequências para a saúde mental da mulher.Essas atitudes são exemplos de um tipo de violência psicológica " +
+                "conhecida pelo nome GASLIGHTING - ou seja, uma situação em que uma pessoa convence a outra de que o que ela está " +
+                "sentido não é real, fazendo-a duvidar da realidade e pensar: 'será que estou ficando louca?' Para saber mais " +
+                "clique <a href='http://www.defensoria.ce.def.br/noticia/voce-esta-ficando-louca-entenda-o-gaslighting-um-dos-tipos-de-violencia-psicologica-contra-a-mulher-2/' target='_blank' class='linkModalInfoPergunta'>aqui</a>.";
+            break;
+        case 8:
+            textoInformacao =
+                "Quando se fala algo do tipo para uma mulher, o que está escrito nas entrelinhas é a ideia de que ela não tem valor " +
+                "e que ela deveria se considerar sortuda por ter alguém (independente dos defeitos da outra pessoa). É mais um exemplo" +
+                "de violência psicológica e relação abusiva, que desqualifica e controla a mulher, forçando-a a se manter nesta relação, " +
+                "o que impacta profundamente a autoestima das mulheres.";
+            break;
+        case 9:
+            textoInformacao =
+                "É importante assumir a responsabilidade pelas nossas próprias atitudes. As vezes, outras pessoas podem agir de um " +
+                "jeito que eu não gosto, e eu posso até sentir raiva, mas o meu sentimento não justifica atitudes violentas. " +
+                "É importante fazer uma autorreflexão para entender o que me deixa irritado, e assumir nossas atitudes para buscar " +
+                "soluções. Se tiver dificuldades, procure ajuda. Ninguém precisa dar conta de tudo sozinho.";
+            break;
+        case 10:
+            textoInformacao =
+                "Ameaças são o tipo de violência mais comum, e apesar de não deixar marcas físicas tem graves impactos emocionais. " +
+                "Além de ser uma forma de violência psicológica, causam preocupação em quem ouve as ameaças e em quem trabalha no combate " +
+                "à violência doméstica, familiar e afetiva. Isso porque a maioria das mulheres assassinadas por seus companheiros foram " +
+                "ameaçadas por ele em algum momento. Para saber mais, clique " +
+                "<a href='https://dossies.agenciapatriciagalvao.org.br/violencia/violencias/feminicidio/#a-ameaca-de-morte-levada-a-serio' target='_blank' class='linkModalInfoPergunta'>aqui</a>.";
+            break;
+        case 11:
+            textoInformacao =
+                "";
+            break;
+        case 12:
+            textoInformacao =
+                "";
+            break;
+        case 13:
+            textoInformacao =
+                "";
+            break;
+        case 14:
+            textoInformacao =
+                "";
+            break;
+        case 15:
+            textoInformacao =
+                "";
+            break;
+        case 16:
+            textoInformacao =
+                "";
+            break;
+        case 17:
+            textoInformacao =
+                "";
+            break;
+        case 18:
+            textoInformacao =
+                "";
+            break;
+        case 19:
+            textoInformacao =
+                "";
+            break;
+        case 20:
+            textoInformacao =
+                "";
+            break;
+    }
+
+    return textoInformacao;
+}
