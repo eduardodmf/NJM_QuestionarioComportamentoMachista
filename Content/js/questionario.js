21eaae9a793f13e8b1e146b3d76016dd3a7ee2ce
Eduardo Deodoro de Moraes Florindo <eduardodmf@outlook.com>
2020-11-06 18:57:55 -0300

    Versão final do site, para revisão do cliente

--- Content/js/questionario.js
+++ Content/js/questionario.js
@@ -10,7 +10,7 @@ function buscarTextoInfoPergunta(numPergunta) {
                 "broder quando ele faz aquela piada machista. Leia mais sobre isso clicando " +
                 "<a href='http://www.compromissoeatitude.org.br/feminicidio-a-ultima-etapa-do-ciclo-da-violencia-contra-a-mulher-por-lourdes-bandeira/' target='_blank' class='linkModalInfoPergunta'> " +
                     "aqui." +
-                "</a>";
+                "</a><br />";
             break;
         case 2:
             textoInformacao =
@@ -20,7 +20,7 @@ function buscarTextoInfoPergunta(numPergunta) {
                 "de exercer tarefas domésticas, cuidar de doentes, criar filhos e qualquer outra tarefa que se atribui à uma mulher. " +
                 "De fato, antropologos como Margaret Mead (<a href='https://pt.wikipedia.org/wiki/Margaret_Mead' target='_blank' class='linkModalInfoPergunta'>https://pt.wikipedia.org/wiki/Margaret_Mead</a>) identificaram diversas situaçoes " +
                 "em que tarefas associadas com masculino e feminino em uma cultura são atribuídas a ou outro.Essas distribuições são " +
-                "arbitrárias e podem ser revistas, especialmente quando estão causando sofrimento ou injustiças.";
+                "arbitrárias e podem ser revistas, especialmente quando estão causando sofrimento ou injustiças. <br />";
             break;
         case 3:
             textoInformacao =
@@ -29,39 +29,42 @@ function buscarTextoInfoPergunta(numPergunta) {
                 "então antes de esbanjar seus conhecimentos, tenha certeza que a sua explicação é necessária-ou desejada. Ter o seu " +
                 "conhecimento subestimado por homens é tão comum para as mulheres que o fenômeno ganhou um nome: Mansplaining " +
                 "(algo como 'explicação do homem'). Para saber mais sobre o assunto, clique " +
-                "<a href='https://revistamarieclaire.globo.com/Comportamento/noticia/2019/03/mansplaining-ou-nem-sempre-sutil-maneira-de-calar-uma-mulher.html;' target='_blank' class='linkModalInfoPergunta'>aqui</a>.";
+                "<a href='https://revistamarieclaire.globo.com/Comportamento/noticia/2019/03/mansplaining-ou-nem-sempre-sutil-maneira-de-calar-uma-mulher.html;' target='_blank' class='linkModalInfoPergunta'>aqui</a>. <br />";
             break;
         case 4:
             textoInformacao =
-                "Primeiro, pra uma paquera ser boa, tem que ser boa para os dois lados. Só quem está sendo ofendido sabe dizer se " +
+                "<b>“Dar uma cantada” em uma mulher nem sempre é inofensivo.</b><br />" +
+                "Primeiro, pra uma paquera ser boa, tem que ser boa para os dois lados.Só quem está sendo ofendido sabe dizer se " +
                 "algo é inofensivo ou não.Então se uma mulher reclama de um elogio, é MUITO importante parar e ouvir o incômodo dela. " +
-                "Se ela está incomodada, a boa intenção ('é só um elogio') pode estar encobrindo um assédio - e se eu estou ignorando a " +
+                "Se ela está incomodada, a boa intenção ('é só um elogio') pode estar encobrindo um assédio-e se eu estou ignorando a " +
                 "vontade dela, tratando o meu desejo como mais importante do que o dela, é exatamente isso que está acontecendo. " +
                 "Tente se colocar no lugar dela. A gente vive em uma sociedade em que as mulheres são frequentemente desrespeitadas e até " +
                 "agredidas na rua e em casa. Vamos fazer nossa parte para diminuir esse mal estar? " +
                 "Para saber mais, clique <a href='https://revistaforum.com.br/blogs/ativismodesofa/eu-tenho-voz-e-digo-cantada-nao-e-elogio/' target='_blank' class='linkModalInfoPergunta'>aqui</a> " +
-                "e/ou <a href='https://www.instagram.com/tv/B1T_aSuAP_7/?igshid=1nzew80hbk044' target='_blank' class='linkModalInfoPergunta'>aqui</a>.";
+                "e/ou <a href='https://www.instagram.com/tv/B1T_aSuAP_7/?igshid=1nzew80hbk044' target='_blank' class='linkModalInfoPergunta'>aqui</a>. <br /><br />";
             break;
         case 5:
             textoInformacao =
+                "<b>Irritar-se com sua parceira não te pedir permissão para fazer algo, pode ser um problema.</b><br />" +
                 "Examine a sua irritação nesse tipo de situação. Fomos criados em uma sociedade em que o homem tem a última palavra. " +
                 "Por trás disso, pode estar embutido uma ideia de hierarquia, ou seja, de que as opiniões e vontades dos homens " +
                 "valem mais do que as da mulher. Se você está se sentindo irritado com uma situação na sua relação, procure formas " +
                 "não-violentas de lidar com isso e, se não conseguir, procure ajuda. Para ler mais, clique " +
-                "<a href='https://brasil.elpais.com/brasil/2018/03/07/politica/1520426823_220468.html' target='_blank' class='linkModalInfoPergunta'>aqui</a>.";
+                "<a href='https://brasil.elpais.com/brasil/2018/03/07/politica/1520426823_220468.html' target='_blank' class='linkModalInfoPergunta'>aqui</a>. <br /><br />";
             break;
         case 6:
             textoInformacao =
+                "<b>Vigiar com quem sua parceira fala, as roupas que ela usa e onde ela vai, não é legal.</b><br />" +
                 "Várias situações de violência doméstica contra a mulher começam com comportamentos controladores, e o próprio " +
                 "comportamento controlador pode ser considerado uma forma de violência psicológica. No final das contas, todo mundo " +
                 "pode escolher suas roupas, suas companhias e os lugares onde quer ir. Para saber mais, clique " +
-                "<a href='https://youtu.be/I-3ocjJTPHg' target='_blank' class='linkModalInfoPergunta'>aqui</a>.";
+                "<a href='https://youtu.be/I-3ocjJTPHg' target='_blank' class='linkModalInfoPergunta'>aqui</a>. <br /><br />";
             break;
         case 7:
             textoInformacao =
                 "Essas frases preocupam porque podem fazer as mulheres duvidarem de si mesmas, causar sofrimento psicológico e " +
                 "várias consequências para a saúde mental da mulher.Essas atitudes são exemplos de um tipo de violência psicológica " +
-                "conhecida pelo nome GASLIGHTING - ou seja, uma situação em que uma pessoa convence a outra de que o que ela está " +
+                "conhecida pelo nome GASLIGHTING-ou seja, uma situação em que uma pessoa convence a outra de que o que ela está " +
                 "sentido não é real, fazendo-a duvidar da realidade e pensar: 'será que estou ficando louca?' Para saber mais " +
                 "clique <a href='http://www.defensoria.ce.def.br/noticia/voce-esta-ficando-louca-entenda-o-gaslighting-um-dos-tipos-de-violencia-psicologica-contra-a-mulher-2/' target='_blank' class='linkModalInfoPergunta'>aqui</a>.";
             break;
@@ -89,43 +92,92 @@ function buscarTextoInfoPergunta(numPergunta) {
             break;
         case 11:
             textoInformacao =
-                "";
+                "Traições(reais ou imaginárias) são frequentemente apontadas como o motivo para crimes contra as mulheres cometidos por seus parceiros. " +
+                "Muitos homens acham que o comportamento da mulher(ou da ex) é uma afronta pessoal à sua honra, ou que o seu valor enquanto homens " +
+                "depende do que a ex faz depois da separação – e isso dá origem a todo tipo de violência e de crimes, desses que a gente vê nos noticiários. " +
+                "Talvez você ache que o seu caso não vai chegar nesse ponto – e talvez não tenha chegado ainda – mas praticamente todos os casos de " +
+                "feminicídio começaram com uma ameaça.Se os seus ciúmes estiverem sendo motivo de muitas brigas na sua relação, pare para refletir sobre " +
+                "isso e procure ajuda enquanto é tempo. " +
+                "Leia mais em: <a href='https://agenciapatriciagalvao.org.br/destaques/por-que-ciume-ainda-e-tao-usado-para-justificar-crimes-contra-mulher/' target='_blank' class='linkModalInfoPergunta'>https://agenciapatriciagalvao.org.br/destaques/por-que-ciume-ainda-e-tao-usado-para-justificar-crimes-contra-mulher/</a>.";
             break;
         case 12:
             textoInformacao =
-                "";
+                "A Lei Maria da Penha não se limita à violência física.Xingamentos e violências verbais entram no que a Lei chama de Violência Psicológica. " +
+                "Apesar de não deixar marcas visíveis, as violências psicológicas causam graves impactos na saúde física e emocional das " +
+                "mulheres –especialmente quando acontecem em uma relação entre pessoas próximas. Embora tanto homens quanto mulheres possam ser xingados, " +
+                "ofendidos humilhados, em relações entre casais é mais comum que as mulheres sejam as vítimas. " +
+                "Repare que os xingamentos contra as mulheres –em especial aqueles considerados mais ofensivos por elas– atribuem a ela comportamentos sexuais " +
+                "ativos: vagabunda, piranha, p ***. Não há equivalente masculino para esses xingamentos, até porque um comportamento sexual ativo não é ofensivo para " +
+                "os homens –pelo contrário. Leve isso em consideração e tente se colocar no lugar delas. Como você se sentiria se fosse xingado de uma maneira que " +
+                "você acha muito ofensiva? E se esse xingamento viesse de alguém muito próximo?" +
+                "<br /><br />" +
+                "Para saber mais, leia <a href='http://www.comciencia.br/violencia-psicologica-causa-danos-graves-ainda-pouco-estudados/' target='_blank' class='linkModalInfoPergunta'>http://www.comciencia.br/violencia-psicologica-causa-danos-graves-ainda-pouco-estudados/</a>.";
             break;
         case 13:
             textoInformacao =
-                "";
+                "Muitos homens justificam utilizar violências para se defender de agressões praticadas por mulheres.Caso você esteja numa situação " +
+                "assim, é importante manter a cabeça fria e achar uma forma não violenta de resolver o conflito.Se afaste, respire fundo algumas vezes. " +
+                "Procure ajuda caso não consiga lidar com a situação. Homens são, de modo geral, fisicamente mais fortes, e a sua resposta num momento de " +
+                "raiva pode ser mais grave do que a agressão que você sofreu em primeiro lugar. Lembre se: uma violência nunca vai ser resolvida com mais violência. " +
+                "Nós, homens, somos ensinados a não levar desaforo pra casa, a ser fortes, a ter a palavra final.Essa socialização tem um " +
+                "preço: nós somos os principais autores de violência física, e somos vítimas de 91, 8 % dos homicídios no nosso país.Pensar sobre " +
+                "soluções diferentes para nossos conflitos é importante para os nossos relacionamentos e para nossa qualidade de vida.";
+
             break;
         case 14:
             textoInformacao =
-                "";
+                "Nem todas violências físicas quebram ossos –algumas agressões são menos óbvias e mais difíceis de perceber. Muitas brigas sérias " +
+                "começam com empurrões, puxões de cabelos, beliscões, etc. Essas agressões –que são um tipo de violência física e podem ser consideradas " +
+                "crime, inclusive sinalizam um crescimento da violência e devem servir como um sinal vermelho de que algo está errado na relação. " +
+                "Se você já se viu fazendo essas coisas, pare, respire e avalie. Procure uma solução diferente para essas brigas e procure ajuda antes " +
+                "que elas fiquem piores.";
             break;
         case 15:
             textoInformacao =
-                "";
+                "Na relação entre duas pessoas, para ser bom para um, tem que ser bom para os dois.Isso é especialmente verdadeiro quando a gente " +
+                "está falando de sexo. É comum o homem pensar que a mulher tem obrigação de satisfazê-lo, mas pense no desejo da sua parceira. " +
+                "Escute. Se você tiver dúvidas de que ela está a fim, pergunte.Se ela parecer desinteressada, cansada, pare. Converse. " +
+                "Tente outro dia. Fazer sexo com alguém que não está com vontade é uma forma de violência sexual: pode não parecer tão grave aos seus " +
+                "olhos, mas pode causar sofrimento para a sua companheira.  ";
             break;
         case 16:
             textoInformacao =
-                "";
+                "É normal a gente se sentir frustrado quando nosso desejo não é atendido, mas lidar com isso de maneira madura faz parte da nossa " +
+                "vida enquanto adultos. É importante lembrar que do outro lado da relação existe um outro ser humano, com desejos, vontades, expectativas " +
+                "e dias difíceis também. Ninguém é obrigado a atender a cada desejo ou capricho meu.Isso é verdade em qualquer relação humana, e é " +
+                "especialmente importante respeitar isso quando a gente está numa relação afetiva com outra pessoa. Pra ser bom, tem que ser bom para os dois.";
             break;
         case 17:
             textoInformacao =
-                "";
+                "Mesmo que você não tenha encostado na sua companheira nessas situações, essas atitudes são violências previstas na Lei Maria da Penha, " +
+                "e podem ter um grande impacto na outra pessoa, especialmente sofrimento emocional. Além disso, pode colocar outras pessoas em " +
+                "situação de risco. Maus tratos contra animais estão associadas a comportamentos bastante preocupantes, e devem servir de alerta, ou seja, " +
+                "motivo para procurar ajuda urgentemente. Procure outras formas de extravasar sua raiva, sem violência. Caso não esteja conseguindo e " +
+                "esses tipo de coisa aconteça mais de uma vez, procure ajuda. Nós homens nos cuidamos menos, procuramos menos terapia e vamos menos ao " +
+                "médico. O Sistema Único de Saúde, por meio de seus órgãos e colaboradores, oferece gratuitamente Práticas integrativas de Saúde. " +
+                "<br /><br />" +
+                "Saiba mais em: <a href='http://www.saude.df.gov.br/praticas-integrativas-de-saude/' target='_blank' class='linkModalInfoPergunta'>http://www.saude.df.gov.br/praticas-integrativas-de-saude/</a>.";
             break;
         case 18:
             textoInformacao =
-                "";
+                "Cada casa e cada família pode ter seus próprios arranjos e combinações (quem trabalha em casa, quem trabalha na rua, quem põe " +
+                "dinheiro na casa, quem cuida dos filhos, etc). Mas é importante que o casal concorde com a maneira como o dinheiro e as despesas " +
+                "familiares são administrados. Só porque você é homem isso não te faz mais apto para lidar com dinheiro ou fazer contas. Sua esposa " +
+                "tem direito de decidir sobre como o dinheiro dela vai ser gasto ou guardado. ";
             break;
         case 19:
             textoInformacao =
-                "";
+                "Pode ser muito traumático para uma criança ver violência entre seus pais. Algumas pesquisas mostram que o sofrimento de uma " +
+                "criança que testemunha brigas de seus pais é equivalente ao de sofrer violências diretamente. Além disso, a criança cresce " +
+                "naturalizando relacionamentos violentos, o que aumenta a chance de entrar neste tipo de relação no futuro –como vítima ou como autor de violência. " +
+                "<br /><br />"
+                "<a href ='http://pepsic.bvsalud.org/scielo.php?script=sci_arttext&pid=S1413-389X2014000400017' target='_blank' class='linkModalInfoPergunta'>http://pepsic.bvsalud.org/scielo.php?script=sci_arttext&pid=S1413-389X2014000400017</a>.";
             break;
         case 20:
             textoInformacao =
-                "";
+                "Álcool e drogas não causam a violência, e eles também não são desculpa. É verdade que algumas drogas diminuem o nosso discernimento " +
+                "e comprometem nossa capacidade de tomar decisões. Mas se você tem usado essa justificativa com frequência, talvez isso seja um sinal " +
+                "de que algo não está bom –seja no seu consumo de álcool/drogas, seja na sua relação.De qualquer modo, procure ajuda. ";
             break;
     }
 
